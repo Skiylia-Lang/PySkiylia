c5a9e446bd72f06a57a90f101188819b32197853
Skiy <skiy@outlook.com>
2021-03-19 12:04:04 +0000

    remove all the things that weren't working

--- test.py
+++ test.py
@@ -1,13 +1,8 @@
  #!/usr/bin/env python
 """This script is purely for testing purposes and will not form a part of the final product"""
 
-import unittests
 
 import PySkiylia
 
-class TestMethods(unittests.TestCase):
-    def test_add(self):
-        self.assertEqual(PySkiylia.test(), "Completed")
-
 if __name__ == "__main__":
-    unittests.main()
+    PySkiylia.test()

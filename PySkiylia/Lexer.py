00a5c1c43b466fbf5048bb3fcf35528a880aaef8
Skiy <skiy@outlook.com>
2021-03-20 14:49:34 +0000

    adding all of the symbol tokens

--- PySkiylia/Lexer.py
+++ PySkiylia/Lexer.py
@@ -5,7 +5,7 @@ import Tokens
 
 #create the Lexer class
 class Lexer:
-    #the function to run at initialisation 
+    #the function to run at initialisation
     def __init__(self, source):
         #fetch the Skiylia class so we have access to it's functions
         from PySkiylia import Skiylia
@@ -27,8 +27,9 @@ class Lexer:
             self.start = self.current
             #fetch the next token
             token = self.tokenFromChar()
-            #append the token to our list of tokens
-            tokens.append(token)
+            #append the token to our list of tokens, provided we recieved one
+            if token:
+                tokens.append(token)
         #add an end of file token
         tokens.append(Tokens.Token("EOF", "", None, self.line+1, 0))
         return tokens
@@ -42,9 +43,94 @@ class Lexer:
             return self.addToken("LeftParenthesis")
         elif c == ")":
             return self.addToken("RightParenthesis")
+        elif c == ":":
+            return self.addToken("Colon")
+        elif c == ",":
+            return self.addToken("Comma")
+        elif c == ".":
+            return self.addToken("Dot")
+        elif c == "-":
+            return self.addToken("Minus")
+        elif c == "+":
+            return self.addToken("Plus")
+        elif c == "*":
+            return self.addToken("Star")
+        elif c == "/":
+            #as division and comments use the same character, check if the next is a comment
+            if self.match("/"):
+                #keep advancing until we find a newline
+                while self.match("\n", peek=True):
+                    self.advance()
+            else:
+                return self.addToken("Slash")
+        elif c == ">":
+            return self.addToken("Greater")
+        elif c == "<":
+            return self.addToken("Less")
+        elif c == "=":
+            return self.addToken("Equal")
+        elif c == "&":
+            return self.addToken("Ampersand")
+        elif c == "|":
+            return self.addToken("Bar")
+        elif c == "!":
+            return self.addToken("Bang")
+        elif c == "\n":
+            #increment the line counter when we reach a newline
+            self.line += 1
+        elif c == '"':
+            #if we have a string identifier
+            return self.findString()
         else:
-            #if nothing matches, throw an error
-            self.skiylia.error(self.line, self.current, "Unexpected character")
+            #check if we have a number to parse
+            if self.isDigit(c):
+                #return the number to be found
+                return self.findNumber()
+            else:
+                #if nothing matches, throw an error
+                self.skiylia.error(self.line, self.current, "Unexpected character")
+
+    #define a way of fetching a string from sourcecode
+    def findString(self):
+        #keep going until we hit the end of the code, or the string terminator
+        while self.match('"', peek=True):
+            #we support multi-line strings, so if we find a newline, increment the line counter
+            if self.match("\n", peek=True):
+                self.line+=1
+            #increment the lexer position
+            self.advance()
+        #if we havent seen the end of the string, raise an error
+        if self.atEnd():
+            self.skiylia.error(self.line, self.current, "Unterminated string")
+        #consume the string closure identifier
+        self.advance()
+        #fetch the contents of the string, removing the leading and trailing identifiers
+        stringVal = self.source[self.start+1:self.current-1]
+        #create the token and return it
+        return self.addToken("String", stringVal)
+
+    #define a way of fetching a number from sourcecode
+    def findNumber(self):
+        #keep going untile we encounter something that isn't a digit
+        while self.isDigit(self.peek()):
+            #advance to the next character
+            self.advance()
+
+        #check if we have a decimal point in the middle
+        if self.match(".", peek=True) and self.isDigit(self.peekNext()):
+            #consume the decimal point
+            self.advance()
+                #keep going untile we encounter something that isn't a digit
+            while self.isDigit(self.peek()):
+                #advance to the next character
+                self.advance()
+        #create a numeric token and return. all numbers are float natively I guess
+        return self.addToken("Number", float(self.source[self.start:self.current]))
+
+    #define a way of checking if the value is a digit
+    def isDigit(self, char):
+        #I realise i could just use the python isdigit, but this allows any 'digit' including squared symbol. Maybe later?
+        return "0" <= char <= "9"
 
     #create a token
     def addToken(self, tokenType, literal=None):
@@ -54,16 +140,34 @@ class Lexer:
         return Tokens.Token(tokenType, text, literal, self.line, self.start)
 
     #advance through the source code if the current character matches what we would expect it to be
-    def match(self, expected=""):
+    def match(self, expected="", peek=False):
         #if we're at the end of the file, we'll want to return false
         if self.atEnd():
             return False
         #if the character is not the expected one, return false
         if self.source[self.current] != expected:
             return False
-        #otherwise, return true and increment the Lexer position
-        self.current+=1
+        #if we are only peeking, don't increment the position
+        if not peek:
+            #otherwise, we want to consume the token
+            self.current+=1
         return True
+
+    #return the current character without advancing
+    def peek(self):
+        #if we're at the end, return an eof
+        if self.atEnd():
+            return '\0'
+        #otherwise return the character
+        return self.source[self.current]
+
+    #return the next character without advancing
+    def peekNext(self):
+        #if we're at the end, or about to be, return an eof
+        if len(self.source) <= self.current+1:
+            return '\0'
+        #otherwise return the character
+        return self.source[self.current+1]
 
     #advance through the source code and return the character
     def advance(self):
--- PySkiylia/Tokens.py
+++ PySkiylia/Tokens.py
@@ -1,11 +1,15 @@
  #!/usr/bin/env python
 """Stores token definitions, may end up moving this elsewhere"""
 
-tokens = ["LeftParenthesis", "RightParenthesis",
-
-        "STRING",
-        "PRINT",
-        "EOF"]
+tokens = [#single character tokens
+            "LeftParenthesis", "RightParenthesis", "Colon", "Comma", "Dot", "Minus", "Plus", "Star", "Slash", "Greater", "Less", "Equal", "Ampersand", "Bar", "Bang",
+            #single or double character Tokens
+            #Literal tokens
+            "String", "Number", "Identifier",
+            #keyword tokens
+            "Class", "Def", "Do", "Elif", "Else", "False", "For", "If", "Null", "Print", "Return", "Self", "Super", "True", "Var","When", "While",
+            #miscellaneous
+            "EOF"]
 
 class Token:
     def __init__(self, type, lexeme, literal, line, char):

0418e58f9f1bc1eeb2f1b16c4187dca207e8c782
Skiy <skiy@outlook.com>
2021-03-21 01:47:03 +0000

    more interpreter code and fixed Parser errors

--- PySkiylia/Interpreter.py
+++ PySkiylia/Interpreter.py
@@ -11,3 +11,122 @@ class Interpreter:
         #fetch the Skiylia class so we have access to it's functions
         from PySkiylia import Skiylia
         self.skiylia = Skiylia()
+
+    #define a way of converting from the literal AST to a runtime value
+    def LiteralExpr(self, expr):
+        return expr.value
+
+    #define a way of unpacking a grouped expression at runtime
+    def GroupingExpr(self, expr):
+        return self.evaluate(expr.expression)
+
+    #define a way of unpacking a Unary
+    def UnaryExpr(self, expr):
+        #fetch the right
+        right = self.evaluate(expr.right)
+        #fetch the operation type
+        optype = expr.operator.type
+        #check whether the optype is recognised
+        if optype == "Minus":
+            self.checkNumber(expr.operator, right)
+            #return the float value negated
+            return -float(right)
+        #check if a logical not
+        elif optype == "Not":
+            #return the not of the operand
+            return not self.isTruthy(right)
+
+        ##if we couldn't get the value
+        return None
+
+    #define a way of unpacking a binary expression
+    def BinaryExpr(self, expr):
+        #fetch the left
+        left = self.evaluate(expr.left)
+        #and the right
+        right = self.evaluate(expr.right)
+        #and the operator
+        optype = expr.operator.type
+        #check the optype
+        if optype == "Greater":
+            #greater comparison
+            return float(left) > float(right)
+        if optype == "Less":
+            #greater comparison
+            return float(left) < float(right)
+        if optype == "NotEqual":
+            #inequality comparison
+            return not self.isEqual(left, right)
+        if optype == "EqualEqual":
+            #equality comparison
+            return self.isEqual(left, right)
+        if optype == "Minus":
+            #subtract if given
+            return float(left) - float(right)
+        elif optype == "Slash":
+            #divide if given
+            return float(left) / float(right)
+        elif optype == "Star":
+            #multiply if given
+            return float(left) * float(right)
+        elif optype == "Plus":
+            #check if left and right are numbers:
+            if (isinstance(left, int) or isinstance(left, float)) and (isinstance(right, int) or isinstance(right, float)):
+                return float(left) + float(right)
+            #check if they are strings to concatenate
+            if isinstance(left, string) and isinstance(right, string):
+                return str(left) + str(right)
+        #if we can't evaluate it at all, return None
+        return None
+
+    #define a way of checking if an object is a number
+    def checkNumber(self, operator, obj):
+        #if it is a float or int, it passes
+        if isinstance(obj, float) or isinstance(obj, int):
+            return
+        #if it's a string
+        elif isinstance(obj, str):
+            #test if it can be converted to a float
+            try:
+                float(obj)
+                return
+            except:
+                pass
+        #raise an error if not
+        raise RuntimeError(operator+" operator requires a number")
+
+    #define a way of checking if something is truthy
+    def isTruthy(self, obj):
+        #If the object is none, false
+        if obj == None:
+            return False
+        #if the object is boolean, return that
+        if isinstance(obj, bool):
+            return obj
+        #if the object is the number zero
+        if obj == 0:
+            return False
+        #everything else is true
+        return True
+
+    #define a way of checking if two values are equal
+    def isEqual(self, a, b):
+        #if they are both null, return true
+        if (a==None) and (b==None):
+            return True
+        #if only one is null, return false
+        if a==None:
+            return False
+        #else return the python equality
+        return a==b
+
+    #define a way of sending the interpreter to the correct method
+    def evaluate(self, expr):
+        ##List of all supported expressions
+        exprs = {"Binary": self.BinaryExpr,
+                 "Grouping": self.GroupingExpr,
+                 "Unary": self.UnaryExpr,}
+        #fetch the class name of the expression provided
+        exprName = expr.__class__.__name__
+        #return the correct method and pass in own value
+        return exprs[exprName](expr)

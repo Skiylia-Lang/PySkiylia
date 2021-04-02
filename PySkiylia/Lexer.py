171bcecefbe97a1f4c88f060aa211080626beb15
Skiy <skiy@outlook.com>
2021-04-02 16:52:40 +0100

    fix error with primitives
    (cherry picked from commit 422242cb59474fd4ba7a83f545e6ae2a9c3f062e)
    
    # Conflicts:
    #	ExampleCode/strings_across_lines.skiy
    #	PySkiylia/Lexer.py

--- PySkiylia/Lexer.py
+++ PySkiylia/Lexer.py
@@ -211,7 +211,11 @@ class Lexer:
 
     #define a way of fetching a string from sourcecode
     def findString(self):
+<<<<<<< HEAD
         si = self.char
+=======
+        startIndent = self.char
+>>>>>>> 422242c (fix error with primitives)
         #keep going until we hit the end of the code, or the string terminator
         while not self.match('"', peek=True):
             #we support multi-line strings, so if we find a newline, increment the line counter
@@ -228,9 +232,13 @@ class Lexer:
         #fetch the contents of the string, removing the leading and trailing identifiers
         stringVal = self.source[self.start+1:self.current-1]
         #remove any whitespace that occured due to newlines
+<<<<<<< HEAD
         if "\n" in stringVal:
             sv = stringVal.split("\n")
             stringVal = " \n".join([sv[0]]+[x[si:] for x in sv[1:]])
+=======
+        stringVal = " \n".join([x.strip() for x in stringVal.split("\n")])
+>>>>>>> 422242c (fix error with primitives)
         #create the token and return it
         return self.addToken("String", stringVal)
 

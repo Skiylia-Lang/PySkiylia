414271108b0e5dbd46efe440d5d26914354c1f6e
Skiy <skiy@outlook.com>
2021-07-03 21:11:26 +0100

    remove pyb
    (cherry picked from commit 05e2cbd269fb488e7d7514c2759c1c419081e968)

--- PySkiylia/PySkiylia.py
+++ PySkiylia/PySkiylia.py
@@ -16,7 +16,7 @@ class Skiylia:
     haderror = False
     version = "v0.8.0"
     title = ""
-    debug = True
+    debug = False
     #run this at initialisation
     def __init__(self, args=""):
         #check if the user has asked for help
